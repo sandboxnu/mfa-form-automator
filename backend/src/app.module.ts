c9680a8c520319c755fa3587895a89f87b149c9d
mibriggs <33757644+mibriggs@users.noreply.github.com>
2023-02-03 16:09:26 -0500

    Configured ConfigService to be more inline with nest reccommendations (#4)

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,11 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { configService } from './config/config.service';
-import "reflect-metadata";
+import { ConfigModule } from '@nestjs/config';
+import { config } from './config/config';
+import { DatabaseConfig } from './config/database.config';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(configService.getTypeOrmConfig())],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `${process.cwd()}/.env.${process.env.NODE_ENV}`,
+      isGlobal: true, 
+      load: [config]
+    }), 
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useClass: DatabaseConfig,
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,21 +1,27 @@
+import "reflect-metadata";
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import { configService } from './config/config.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  const PORT = configService.get<number>('PORT') || 3000; 
+  const isDev: boolean = configService.get<boolean>('isDev');
 
-  if (!configService.isProduction()) {
-      const config = new DocumentBuilder()
-      .setTitle('Cool Cats example')
-      .setDescription('The cats API description')
-      .setVersion('1.0')
-      .addTag('cats')
-      .build();
-      const document = SwaggerModule.createDocument(app, config);
-      SwaggerModule.setup('api', app, document);
-      await app.listen(3001);
+  if (isDev) {
+    const config = new DocumentBuilder()
+    .setTitle('Cool Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
   }
+  
+  await app.listen(PORT);
+  console.log(`App is listening on port: ${PORT}`);
 }
 bootstrap();
