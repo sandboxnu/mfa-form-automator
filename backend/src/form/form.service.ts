fd58a7cf6aab75b7f3b85de98a596d5127b20862
mibriggs <33757644+mibriggs@users.noreply.github.com>
2023-02-12 15:01:27 -0500

    created post endpoint to create form (#8)
    * created post endpoint to create form
    
    * log all created forms
    
    ---------
    
    Co-authored-by: Kaiyang Zheng <kaiyang.zheng@gmail.com>
    Co-authored-by: Elvin Cheng <elvincheng3@gmail.com>

---
+++ backend/src/form/form.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Form } from 'src/models/form.entity';
+import { Repository } from 'typeorm';
+import { CreateFormDto, FormDto } from './form.dto';
+
+@Injectable()
+export class FormService {
+  constructor(
+    @InjectRepository(Form) private formRepository: Repository<Form>,
+  ) {}
+
+  createForm(createFormDto: CreateFormDto) {
+    //TODO signatureChanLinkHead should be built out
+    const formDto: FormDto = {
+      name: createFormDto.name,
+      pdfLink: createFormDto.pdfLink,
+      signatureChainLinkHead: null,
+      formInstances: [],
+    };
+
+    const form: Form = this.formRepository.create(formDto);
+    return this.formRepository.save(form);
+  }
+
+  async findAllForms() {
+    console.log(await this.formRepository.find());
+  }
+}
--- backend/src/models/form.entity.ts
+++ backend/src/models/form.entity.ts
@@ -1,25 +1,30 @@
-import { Column, Entity, JoinColumn, OneToMany, OneToOne, PrimaryGeneratedColumn } from "typeorm";
-import { FormInstance } from "./formInstance.entity";
-import { SignatureChainLink } from "./signatureChainLink.entity";
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  OneToMany,
+  OneToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { FormInstance } from './formInstance.entity';
+import { SignatureChainLink } from './signatureChainLink.entity';
 
 @Entity()
+// TODO: Forms should be unique?
 export class Form {
-    @PrimaryGeneratedColumn()
-    id: number
-    
-    @Column()
-    name: string
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column()
-    pdfLink: string
+  @Column()
+  name: string;
 
-    @OneToOne(() => SignatureChainLink, (signatureChainLink) => signatureChainLink.form)
-    @JoinColumn({ name: "signatureChainLinkHeadId" })
-    signatureChainLinkHead: SignatureChainLink
+  @OneToOne(() => SignatureChainLink, (signatureChainLink) => signatureChainLink.form)
+  @JoinColumn({ name: "signatureChainLinkHeadId" })
+  signatureChainLinkHead: SignatureChainLink
 
-    @Column({ type: "int", nullable: true })
-    signatureChainLinkHeadId: number
+  @Column({ type: "int", nullable: true })
+  signatureChainLinkHeadId: number
 
-    @OneToMany(() => FormInstance, (formInstance) => formInstance.formType)
-    formInstances: FormInstance[]
+  @OneToMany(() => FormInstance, (formInstance) => formInstance.formType)
+  formInstances: FormInstance[]
 }
