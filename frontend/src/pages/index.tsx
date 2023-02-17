700a2b162ee4bf9e5f80b7cbd27f87501f803593
Angela Weigl <ahweigl@gmail.com>
2023-02-16 22:27:27 -0500

    in progress nav bar

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,9 +1,11 @@
 import { Button } from '@chakra-ui/react'
+import { NavBar } from '@/components/NavBar'
 
 export default function Home() {
   return (
     <>
       <Button>Component Test</Button>
+      <NavBar/>
     </>
   )
 }
