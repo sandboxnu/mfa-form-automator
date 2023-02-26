cac5feffdbaa5ae7659b8be8e52e1b30e78148a2
Angela Weigl <ahweigl@gmail.com>
2023-02-26 16:29:53 -0500

    broken lol

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,9 +1,13 @@
 import { NavBar } from '@/components/NavBar'
+import {useState} from 'react'; 
 
 export default function Home() {
+
+  const [route, setRoute] = useState('/home'); 
+
   return (
     <>
-      <NavBar/>
+      <NavBar route={route} setRoute={setRoute}/>
     </>
   )
 }
