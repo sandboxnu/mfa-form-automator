66b1e893dba5e4fa234977eca7419823686067ce
Elvin Cheng <elvincheng3@gmail.com>
2023-03-19 12:22:03 -0400

    rough draft

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,9 +1,19 @@
-import { NavBar } from "./NavBar"
+import { Box, Flex, HStack, Spacer, VStack } from "@chakra-ui/react";
+import { NavBar } from "./NavBar";
+import { TopBar } from "./TopBar";
 
 // Common layout component for all pages
-export const Layout = ({children}: {children: any}) => {
-  return <>
-    <NavBar/>
-    {children}
-  </>
-}
+export const Layout = ({ children }: { children: any }) => {
+  return (
+    <>
+      <VStack>
+        <TopBar />
+        <HStack minW="100vw">
+          <NavBar />
+          <Spacer />
+        </HStack>
+      </VStack>
+      {children}
+    </>
+  );
+};
---
+++ frontend/src/components/TopBar.tsx
@@ -0,0 +1,48 @@
+import { MFALogoIcon, ProfileIcon, SearchIcon } from "@/static/icons";
+import {
+  Box,
+  HStack,
+  IconButton,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Select,
+  Spacer,
+  Text,
+} from "@chakra-ui/react";
+
+export const TopBar: React.FC = () => {
+  return (
+    <Box padding={10} w="100vw" h="100">
+      <HStack>
+        <MFALogoIcon boxSize={20} />
+        <Spacer minW={200} />
+        <InputGroup maxW="600">
+          <Select
+            maxW="120"
+            backgroundColor="lightgrey"
+            borderTopRightRadius="0"
+            borderBottomRightRadius="0"
+          >
+            <option value="option1">Option 1</option>
+            <option value="option2">Option 2</option>
+            <option value="option3">Option 3</option>
+          </Select>
+          <InputRightElement pointerEvents="none" children={<SearchIcon />} />
+          <Input
+            placeholder="Search"
+            borderTopLeftRadius="0"
+            borderBottomLeftRadius="0"
+          />
+        </InputGroup>
+        <Spacer />
+        <Text>Welcome back, User!</Text>
+        <IconButton
+          aria-label="Visit profile"
+          icon={<ProfileIcon boxSize={7} />}
+          colorScheme="none"
+        />
+      </HStack>
+    </Box>
+  );
+};
