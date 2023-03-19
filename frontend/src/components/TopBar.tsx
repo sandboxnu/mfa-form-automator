62c6d7fc6bb2303c8e4f7fbb83aeb97e555a7dfc
Elvin Cheng <elvincheng3@gmail.com>
2023-03-19 16:03:45 -0400

    update select

--- frontend/src/components/TopBar.tsx
+++ frontend/src/components/TopBar.tsx
@@ -24,7 +24,7 @@ export const TopBar: React.FC = () => {
             borderTopRightRadius="0"
             borderBottomRightRadius="0"
           >
-            <option value="option1">Option 1</option>
+            <option value="all_forms">All Forms</option>
             <option value="option2">Option 2</option>
             <option value="option3">Option 3</option>
           </Select>
