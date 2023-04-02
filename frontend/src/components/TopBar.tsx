aa5daa71c2d45f9b5d5ea661d1fe887af3b6920a
Elvin Cheng <elvincheng3@gmail.com>
2023-04-02 16:05:47 -0400

    Implement top bar (#14)
    * rough draft
    
    * update select
    
    * sizing
    
    * fix mfa logo
    
    * conditionally hide greeting
    
    * move children
    
    * fixed bars
    
    * use template
    
    * increase box size
    
    * search icon
    
    * cleanup
    
    * remove unused imports
    
    * lint
    
    * put navbar and topbar into seperate files, changed styling in accordance with figma
    
    ---------
    
    Co-authored-by: Kaiyang Zheng <kaiyang.zheng@gmail.com>
    Co-authored-by: Isabelle Papa <101610824+ispapa@users.noreply.github.com>

---
+++ frontend/src/components/TopBar.tsx
@@ -0,0 +1,77 @@
+import { MFALogoIcon, ProfileIcon, SearchIcon } from "@/static/icons";
+import {
+  Box,
+  Hide,
+  IconButton,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Select,
+  Text,
+  Flex
+} from "@chakra-ui/react";
+
+export const TopBar: React.FC = () => {
+  return (
+    <Flex
+      as="header"
+      align="center"
+      pos="fixed"
+      w="full"
+      px="4"
+      bg="white"
+      _dark={{
+        bg: "gray.800",
+      }}
+      borderBottomWidth="1px"
+      color="inherit"
+      h="136"
+    >
+      <Box minWidth={302}>
+        <Flex px="4" py="5" align="left">
+          <MFALogoIcon height="51px" width="220px" />
+        </Flex>
+      </Box>
+      <InputGroup
+        display={{
+          md: "flex",
+        }}
+      >
+        <Select
+          minW="120"
+          maxW="120"
+          backgroundColor="lightgrey"
+          borderTopRightRadius="0"
+          borderBottomRightRadius="0"
+        >
+          <option value="all_forms">All Forms</option>
+          <option value="option2">Option 2</option>
+          <option value="option3">Option 3</option>
+        </Select>
+        <Input
+          borderTopLeftRadius="0"
+          borderBottomLeftRadius="0"
+          placeholder="Search all forms"
+        />
+        <InputRightElement pointerEvents="none">
+          <SearchIcon boxSize="7" color="grey" />
+        </InputRightElement>
+      </InputGroup>
+
+      <Flex align="center" pl="10">
+        <Hide breakpoint="(max-width: 1000px)">
+          <Text minW="200" align="right">
+            Welcome back, User!
+          </Text>
+        </Hide>
+        <IconButton
+          pl="12px"
+          pr="40px"
+          aria-label="Visit profile"
+          icon={<ProfileIcon boxSize={7} />}
+          colorScheme="none"
+        />
+      </Flex>
+    </Flex>
+  );
+};
