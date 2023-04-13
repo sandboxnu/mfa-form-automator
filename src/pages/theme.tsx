64d065dbfa3cc7373d2ee5b943632b55dcac891b
Elvin Cheng <elvincheng3@gmail.com>
2023-04-12 21:05:38 -0400

    add hover and font

---
+++ src/pages/theme.tsx
@@ -0,0 +1,10 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  fonts: {
+    heading: `'Hanken Grotesk', sans-serif`,
+    body: `'Hanken Grotesk', sans-serif`,
+  },
+});
+
+export default theme;
