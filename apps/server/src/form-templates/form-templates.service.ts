9c2e457327b56fe0f3006048c4d1650ce71457fa
Kaiyang Zheng <kaiyang.zheng@gmail.com>
2023-09-21 19:23:13 -0400

    added dtos for form template

--- apps/server/src/form-templates/form-templates.service.ts
+++ apps/server/src/form-templates/form-templates.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@nestjs/common';
-// import { CreateFormTemplateDto } from './dto/create-form-template.dto';
-// import { UpdateFormTemplateDto } from './dto/update-form-template.dto';
+import { CreateFormTemplateDto } from './dto/create-form-template.dto';
+import { UpdateFormTemplateDto } from './dto/update-form-template.dto';
+import { PrismaService } from '../prisma/prisma.service';
+import { Signature } from '@prisma/client';
 
 @Injectable()
 export class FormTemplatesService {
+  constructor(private prisma: PrismaService) {}
+
+  /**
+   * Create a new form template.
+   * @param createFormTemplateDto create form template dto
+   * @returns the created employee, hydrated
+   */
+  async create(createFormTemplateDto: CreateFormTemplateDto) {
+    const newFormTemplate = await this.prisma.formTemplate.create({
+      data: {
+        name: createFormTemplateDto.name,
+        formDocLink: createFormTemplateDto.formDocLink,
+        signatureFields: { create: createFormTemplateDto.signatureFields },
+      }
+    })
+  }
+
   // create(createFormTemplateDto: CreateFormTemplateDto) {
   //   return 'This action adds a new formTemplate';
   // }
