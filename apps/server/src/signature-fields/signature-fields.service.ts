ca90b89ebed574334c5ee5c7958272e72cc23456
ispapa <belle07704@gmail.com>
2023-09-24 16:20:30 -0400

    Signature field service

--- apps/server/src/signature-fields/signature-fields.service.ts
+++ apps/server/src/signature-fields/signature-fields.service.ts
@@ -1,22 +1,80 @@
 import { Injectable } from '@nestjs/common';
-// import { CreateSignatureFieldDto } from './dto/create-signature-field.dto';
-// import { UpdateSignatureFieldDto } from './dto/update-signature-field.dto';
+import { PrismaService } from '../prisma/prisma.service';
+import { CreateSignatureFieldDto } from './dto/create-signature-field.dto';
+import { UpdateSignatureFieldDto } from './dto/update-signature-field.dto';
 
 @Injectable()
 export class SignatureFieldsService {
-  // create(createSignatureFieldDto: CreateSignatureFieldDto) {
-  //   return 'This action adds a new signatureField';
-  // }
-  // findAll() {
-  //   return `This action returns all signatureFields`;
-  // }
-  // findOne(id: number) {
-  //   return `This action returns a #${id} signatureField`;
-  // }
-  // update(id: number, updateSignatureFieldDto: UpdateSignatureFieldDto) {
-  //   return `This action updates a #${id} signatureField`;
-  // }
-  // remove(id: number) {
-  //   return `This action removes a #${id} signatureField`;
-  // }
+  constructor(private prisma: PrismaService) {}
+
+  /**
+   * Create a new signature field.
+   * @param createSignatureFieldDto create signature field dto
+   * @returns the created signature field, hydrated
+   */
+  async create(createSignatureFieldDto: CreateSignatureFieldDto) {
+    const newSignatureField = await this.prisma.signatureField.create({
+      data: {
+        name: createSignatureFieldDto.name, 
+        order: createSignatureFieldDto.order, 
+        signerPositionId: createSignatureFieldDto.signerPositionId, 
+        formTemplateId: createSignatureFieldDto.formTemplateId
+      },
+    });
+    return newSignatureField;
+  }
+
+  /**
+   * Retrieve all signature fields.
+   * @param limit the number of signature fields we want to retrieve (optional)
+   * @returns all signature fields, hydrated
+   */
+  async findAll(limit?: number) {
+    const signatureFields = await this.prisma.signatureField.findMany({
+      take: limit,
+    });
+    return signatureFields;
+  }
+
+  /**
+   * Retrieve a signature field by id.
+   * @param id the signature field id
+   * @returns the selected signature field, hydrated
+   */
+  async findOne(id: string) {
+    const signatureField = await this.prisma.signatureField.findFirstOrThrow({
+      where: {
+        id: id,
+      },
+    });
+    return signatureField;
+  }
+
+  /**
+   * Update a signature field.
+   * @param id the signature field id
+   * @param updateSignatureFieldDto update signature field dto
+   * @returns the updated signature field, hydrated
+   */
+  async update(id: string, updateSignatureFieldDto: UpdateSignatureFieldDto) {
+    const updatedSignatureField = this.prisma.signatureField.update({
+      where: {
+        id: id,
+      },
+      data: UpdateSignatureFieldDto,
+    });
+    return updatedSignatureField;
+  }
+
+  /**
+   * Remove a signature field.
+   * @param id the signature field id
+   */
+  async remove(id: string) {
+    await this.prisma.signatureField.delete({
+      where: {
+        id: id,
+      },
+    });
+  }
 }
