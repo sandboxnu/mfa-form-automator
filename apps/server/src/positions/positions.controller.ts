3bb77fe7af524f9eeaf3140795b32e16783ce168
Anshul Shirude <34725764+AnshulShirude@users.noreply.github.com>
2023-09-26 23:55:21 -0400

    need to check if it's working

--- apps/server/src/positions/positions.controller.ts
+++ apps/server/src/positions/positions.controller.ts
@@ -1,45 +1,124 @@
 import {
   Controller,
-  // Get,
-  // Post,
-  // Body,
-  // Patch,
-  // Param,
-  // Delete,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  NotFoundException,
+  Query,
 } from '@nestjs/common';
 import { PositionsService } from './positions.service';
-// import { CreatePositionDto } from './dto/create-position.dto';
-// import { UpdatePositionDto } from './dto/update-position.dto';
+import { CreatePositionDto } from './dto/create-position.dto';
+import { UpdatePositionDto } from './dto/update-position.dto';
+import {
+  ApiBadRequestResponse,
+  ApiCreatedResponse,
+  ApiForbiddenResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiTags,
+  ApiUnprocessableEntityResponse,
+} from '@nestjs/swagger';
+import { PositionEntity } from './entities/position.entity';
+import { Prisma } from '@prisma/client';
+import { AppErrorMessage } from '../app.errors';
+import { PositionsErrorMessage } from './positions.errors';
 
+@ApiTags('positions')
 @Controller('positions')
 export class PositionsController {
   constructor(private readonly positionsService: PositionsService) {}
 
-  // @Post()
-  // create(@Body() createPositionDto: CreatePositionDto) {
-  //   return this.positionsService.create(createPositionDto);
-  // }
 
-  // @Get()
-  // findAll() {
-  //   return this.positionsService.findAll();
-  // }
+  @Post()
+  @ApiCreatedResponse({ type: PositionEntity })
+  @ApiForbiddenResponse({ description: AppErrorMessage.FORBIDDEN })
+  @ApiUnprocessableEntityResponse({
+    description: AppErrorMessage.UNPROCESSABLE_ENTITY,
+  })
+  @ApiBadRequestResponse({ description: AppErrorMessage.UNPROCESSABLE_ENTITY })
+  async create(@Body() createPositionDto: CreatePositionDto) {
+    const newPosition = await this.positionsService.create(createPositionDto);
+    return new PositionEntity(newPosition);
+  }
 
-  // @Get(':id')
-  // findOne(@Param('id') id: string) {
-  //   return this.positionsService.findOne(+id);
-  // }
+  @Get()
+  @ApiOkResponse({ type: [PositionEntity] })
+  @ApiForbiddenResponse({ description: AppErrorMessage.FORBIDDEN })
+  @ApiBadRequestResponse({ description: AppErrorMessage.UNPROCESSABLE_ENTITY })
+  async findAll(@Query('limit') limit?: number) {
+    const positions = await this.positionsService.findAll(limit);
+    return positions.map((position) => new PositionEntity(position));
+  }
 
-  // @Patch(':id')
-  // update(
-  //   @Param('id') id: string,
-  //   @Body() updatePositionDto: UpdatePositionDto,
-  // ) {
-  //   return this.positionsService.update(+id, updatePositionDto);
-  // }
+  @Get(':id')
+  @ApiOkResponse({ type: PositionEntity })
+  @ApiForbiddenResponse({ description: AppErrorMessage.FORBIDDEN })
+  @ApiNotFoundResponse({ description: AppErrorMessage.NOT_FOUND })
+  @ApiBadRequestResponse({ description: AppErrorMessage.UNPROCESSABLE_ENTITY })
+  async findOne(@Param('id') id: string) {
+    const position = await this.positionsService.findOne(id);
+    if (position == null) {
+      console.log(PositionsErrorMessage.POSITION_NOT_FOUND);
+      throw new NotFoundException(
+        PositionsErrorMessage.POSITION_NOT_FOUND_CLIENT,
+      );
+    }
+    return new PositionEntity(position);
+  }
 
-  // @Delete(':id')
-  // remove(@Param('id') id: string) {
-  //   return this.positionsService.remove(+id);
-  // }
+  @Patch(':id')
+  @ApiOkResponse({ type: PositionEntity })
+  @ApiForbiddenResponse({ description: AppErrorMessage.FORBIDDEN })
+  @ApiNotFoundResponse({ description: AppErrorMessage.NOT_FOUND })
+  @ApiUnprocessableEntityResponse({
+    description: AppErrorMessage.UNPROCESSABLE_ENTITY,
+  })
+  @ApiBadRequestResponse({ description: AppErrorMessage.UNPROCESSABLE_ENTITY })
+  async update(
+    @Param('id') id: string,
+    @Body() updatePositionDto: UpdatePositionDto,
+  ) 
+  {
+    try {
+      const updatedPosition = await this.positionsService.update(
+        id,
+        updatePositionDto,
+      );
+      return new PositionEntity(updatedPosition);
+    } catch (e) {
+      if (e instanceof Prisma.PrismaClientKnownRequestError) {
+        if (e.code === 'P2025') {
+          console.log(PositionsErrorMessage.POSITION_NOT_FOUND);
+          throw new NotFoundException(
+            PositionsErrorMessage.POSITION_NOT_FOUND_CLIENT,
+          );
+        }
+      }
+      throw e;
+    }
+  }
+
+  @Delete(':id')
+  @ApiOkResponse()
+  @ApiForbiddenResponse({ description: AppErrorMessage.FORBIDDEN })
+  @ApiNotFoundResponse({ description: AppErrorMessage.NOT_FOUND })
+  @ApiBadRequestResponse({ description: AppErrorMessage.UNPROCESSABLE_ENTITY })
+  async remove(@Param('id') id: string) {
+    try {
+      await this.positionsService.remove(id);
+    } catch (e) {
+      if (e instanceof Prisma.PrismaClientKnownRequestError) {
+        if (e.code === 'P2025') {
+          console.log(PositionsErrorMessage.POSITION_NOT_FOUND);
+          throw new NotFoundException(
+            PositionsErrorMessage.POSITION_NOT_FOUND_CLIENT,
+          );
+        }
+      }
+      throw e;
+    }
+  }
 }
---
+++ apps/server/src/positions/positions.errors.ts
@@ -0,0 +1,4 @@
+export enum PositionsErrorMessage {
+  POSITION_NOT_FOUND = 'Position could not be found with this id',
+  POSITION_NOT_FOUND_CLIENT = 'Position could not be found',
+}
