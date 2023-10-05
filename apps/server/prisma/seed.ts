9315312f8e0e519ab3637044fc26682a69bb0ec3
Angela Weigl <ahweigl@gmail.com>
2023-10-05 18:43:52 -0400

    fixed seeding

--- apps/server/prisma/seed.ts
+++ apps/server/prisma/seed.ts
@@ -1,20 +1,22 @@
 import { PrismaClient } from '@prisma/client'
+import { v4 as uuidv4 } from 'uuid';
 const prisma = new PrismaClient()
 async function main() {
   const formTemplate1 =  await prisma.formTemplate.upsert({
-    where: {id: '1', name: 'Form Template 1'},
+    where: {id: uuidv4(), name: 'Form Template 1'},
     update: {},
     create: {
-      id: '1',
+
+      id: uuidv4(),
       name: 'Form Template 1',
       formDocLink: 'https://www.mfa.org/'
     },
   })
   const signaturefield1 = await prisma.signatureField.upsert({
-    where: {name: 'John Smith', id: '1234'},
+    where: {name: 'John Smith', id: '550e8400e29b41d4a716446655440000'},
     update: {},
     create: {
-      id: '1234',
+      id: '550e8400e29b41d4a716446655440000',
       name: 'John Smith',
       order: 1,
       formTemplate: {
