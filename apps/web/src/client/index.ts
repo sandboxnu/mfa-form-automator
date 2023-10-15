fd5effc233fa502fd356c12254dd95c99639b5d5
Elvin Cheng <elvincheng3@gmail.com>
2023-10-15 16:20:05 -0400

    gen client

--- apps/web/src/client/index.ts
+++ apps/web/src/client/index.ts
@@ -7,6 +7,7 @@ export { CancelablePromise, CancelError } from './core/CancelablePromise';
 export { OpenAPI } from './core/OpenAPI';
 export type { OpenAPIConfig } from './core/OpenAPI';
 
+export type { CreateDepartmentDto } from './models/CreateDepartmentDto';
 export type { CreateEmployeeDto } from './models/CreateEmployeeDto';
 export type { CreateFormInstanceDto } from './models/CreateFormInstanceDto';
 export type { CreateFormTemplateDto } from './models/CreateFormTemplateDto';
@@ -19,6 +20,7 @@ export type { FormTemplateBaseEntity } from './models/FormTemplateBaseEntity';
 export type { FormTemplateEntity } from './models/FormTemplateEntity';
 export type { PositionEntity } from './models/PositionEntity';
 export type { SignatureFieldEntity } from './models/SignatureFieldEntity';
+export type { UpdateDepartmentDto } from './models/UpdateDepartmentDto';
 export type { UpdateEmployeeDto } from './models/UpdateEmployeeDto';
 export type { UpdateFormInstanceDto } from './models/UpdateFormInstanceDto';
 export type { UpdateFormTemplateDto } from './models/UpdateFormTemplateDto';
@@ -26,6 +28,7 @@ export type { UpdatePositionDto } from './models/UpdatePositionDto';
 export type { UpdateSignatureFieldDto } from './models/UpdateSignatureFieldDto';
 
 export { DefaultService } from './services/DefaultService';
+export { DepartmentsService } from './services/DepartmentsService';
 export { EmployeesService } from './services/EmployeesService';
 export { FormInstancesService } from './services/FormInstancesService';
 export { FormTemplatesService } from './services/FormTemplatesService';
