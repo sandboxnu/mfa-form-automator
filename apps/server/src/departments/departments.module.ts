cc939e5ad61a8f80214eeb26a78df1b8f03dfde6
Elvin Cheng <elvincheng3@gmail.com>
2023-10-17 12:38:46 -0400

    remove import

--- apps/server/src/departments/departments.module.ts
+++ apps/server/src/departments/departments.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { DepartmentsService } from './departments.service';
 import { DepartmentsController } from './departments.controller';
 import { PrismaModule } from '../prisma/prisma.module';
-import { JwtStrategy } from '@server/auth/strategies/jwt.strategy';
 
 @Module({
   controllers: [DepartmentsController],
-  providers: [DepartmentsService, JwtStrategy],
+  providers: [DepartmentsService],
   imports: [PrismaModule],
 })
 export class DepartmentsModule {}
