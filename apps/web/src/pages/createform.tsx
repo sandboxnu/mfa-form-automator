63393a06d003dfe0441080a91ec10371a53d150a
Angela Weigl <ahweigl@gmail.com>
2023-10-16 22:02:46 -0400

    create form button routing

---
+++ apps/web/src/pages/createform.tsx
@@ -0,0 +1,11 @@
+import { Box, Button, Input } from '@chakra-ui/react';
+
+const CreateForm = () => {
+  return (
+    <Box padding="20px">
+      <h1>Create Form</h1>
+    </Box>
+  );
+};
+
+export default CreateForm;
