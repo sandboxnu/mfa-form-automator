bc37f3891a51795f3bdb734385944b0ee3f8c3de
ispapa <belle07704@gmail.com>
2023-10-19 19:33:46 -0400

    add back to overview link + some text

--- apps/web/src/pages/createform.tsx
+++ apps/web/src/pages/createform.tsx
@@ -1,10 +1,24 @@
-import { Box, Button, Input } from '@chakra-ui/react';
+import {Flex, Link, Text, VStack} from '@chakra-ui/react';
+import { LeftArrowIcon } from '@web/static/icons';
 
 const CreateForm = () => {
   return (
-    <Box padding="20px">
-      <h1>Create Form</h1>
-    </Box>
+    <Flex flexDirection="column" marginLeft="49px"> 
+      <Link href='/'>
+        <Flex alignItems="center" marginTop="42px" marginBottom="22px">
+          <LeftArrowIcon width="10px" height="10px" marginLeft="4px" marginRight="4px" />
+          <Text fontWeight="700" fontSize="16px" color="#4C658A">
+            Back to Overview
+          </Text>
+        </Flex>
+      </Link>
+      <Text fontWeight="1000" fontSize="27px" color="black" paddingBottom="41px">
+        [Draft] Form Name Placeholder
+      </Text>
+      <Text fontWeight="700" fontSize="20px" color="black" paddingBottom="10px">
+        Form Type
+      </Text>
+    </Flex>
   );
 };
 
