1272eb44c9470ef39fd86fb8c6653fc0a5bbc23d
Elvin Cheng <elvincheng3@gmail.com>
2023-10-22 18:25:19 -0400

    setup cookie

--- apps/server/src/app.controller.ts
+++ apps/server/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, UseGuards, Request } from '@nestjs/common';
+import { Controller, Get, Post, UseGuards, Request, Res } from '@nestjs/common';
 import { AppService } from './app.service';
 import {
   ApiOkResponse,
@@ -12,6 +12,7 @@ import { JwtEntity } from './auth/entities/jwt.entity';
 import { LocalAuthGuard } from './auth/guards/local-auth.guard';
 import { EmployeeEntity } from './employees/entities/employee.entity';
 import { AuthService } from './auth/auth.service';
+import { Response as ResponseType } from 'express';
 
 @Controller()
 export class AppController {
@@ -41,7 +42,22 @@ export class AppController {
       },
     },
   })
-  async login(@Request() req: EmployeeEntity) {
-    return this.authService.login(req);
+  async login(
+    @Request() req: EmployeeEntity,
+    @Res({ passthrough: true }) response: ResponseType,
+  ) {
+    const frontendDomain = process.env.FRONTEND_DOMAIN;
+    const jwtToken = await this.authService.login(req);
+    response.cookie('jwt', jwtToken.access_token, {
+      httpOnly: true,
+      domain: frontendDomain,
+    });
+    return { jwt: jwtToken };
+  }
+
+  @Get('/auth/logout')
+  @ApiOkResponse({ type: undefined })
+  async logout(@Res({ passthrough: true }) res: ResponseType) {
+    res.cookie('token', '', { expires: new Date() });
   }
 }
--- apps/server/src/auth/strategies/jwt.strategy.ts
+++ apps/server/src/auth/strategies/jwt.strategy.ts
@@ -2,12 +2,16 @@ import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt } from 'passport-jwt';
 import { Strategy } from 'passport-jwt';
+import { Request as RequestType } from 'express';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
     super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        JwtStrategy.extractJWT,
+        ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ]),
       ignoreExpiration: false,
       secretOrKey: process.env.JWT_SECRET,
     });
@@ -15,5 +19,16 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
 
   async validate(payload: any) {
     return { id: payload.sub, email: payload.email };
+  }
+
+  private static extractJWT(req: RequestType): string | null {
+    if (
+      req.cookies &&
+      'token' in req.cookies &&
+      req.cookies.user_token.length > 0
+    ) {
+      return req.cookies.token;
+    }
+    return null;
   }
 }
