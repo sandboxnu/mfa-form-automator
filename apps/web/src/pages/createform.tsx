fa692f6454e782c9c4ed86d410f20e3695bcefc4
Angela Weigl <ahweigl@gmail.com>
2023-10-26 16:11:10 -0400

    switched form type dropdown to chakra-react-select component

--- apps/web/src/pages/createform.tsx
+++ apps/web/src/pages/createform.tsx
@@ -1,42 +1,34 @@
-import { Box, Button, Flex, Select, Text } from '@chakra-ui/react';
+import { Box, Button, Flex, Text } from '@chakra-ui/react';
 import { LeftArrowIcon } from '@web/static/icons';
 import { useState } from 'react';
 import Link from 'next/link';
 import { DropdownDownArrow, DropdownUpArrow } from 'apps/web/src/static/icons';
 // TODO make leadership/dept head selects searchable using chakra-react-select
-// import { Select } from 'chakra-react-select';
+import { GroupBase, OptionsOrGroups, Select } from 'chakra-react-select';
 
 const CreateForm = () => {
+  interface Option {
+    value: string;
+    label: string;
+  }
+  const [selectedForm, setSelectedForm] = useState<Option | null>(null);
+  // const [selectedForm, setSelectedForm] = useState('');
+  const options: Option[] = [
+    // { value: '', label: 'Select Form Template' },
+    { value: 'Form 1', label: 'Form 1' },
+    { value: 'Form 2', label: 'Form 2' },
+    { value: 'Form 3', label: 'Form 3' },
+  ];
 
-  const [selectedForm, setSelectedForm] = useState('');
-  const [isDropdownFocused, setIsDropdownFocused] = useState(false);
-  const [isDropdownIconChanged, setIsDropdownIconChanged] = useState(false);
 
-  const handleFormChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    setIsDropdownIconChanged(false);
-    setSelectedForm(event.target.value);
+
+  // const handleFormChange = (selectedOption: Option) => {
+  //   setSelectedForm(selectedOption.value);
+  // };
+  const handleFormChange = (selectedOption: Option) => {
+    setSelectedForm(selectedOption);
   };
 
-  const handleDropdownFocus = () => {
-    setIsDropdownFocused(true);
-  };
-
-  const handleDropdownBlur = () => {
-    setIsDropdownFocused(false);
-  };
-
-  const handleDropdownClick = () => {
-    setIsDropdownIconChanged(true);
-  };
-
-  const renderDropdownIcon = () => {
-    if (isDropdownFocused) {
-      return <DropdownUpArrow />;
-    }
-    else {
-      return <DropdownDownArrow />;
-    }
-  };
 
 
   return (
@@ -52,7 +44,7 @@ const CreateForm = () => {
         </Link>
       </Flex>
       <Text fontWeight="1000" fontSize="27px" color="black" paddingBottom="41px">
-        {[selectedForm ? `[New] ${selectedForm}` : "[New] Form"]}
+      {selectedForm ? selectedForm.label : "[New] Form"}
       </Text>
 
       <Flex flexDirection="row">
@@ -60,19 +52,14 @@ const CreateForm = () => {
           <Text fontWeight="700" fontSize="20px" color="black">
             Form Type
           </Text>
-          {/* TO DO fix icon so it changes back to down arrow immediately on clicking dropdown option */}
-          <Select width="496px" height="40px" marginTop="12px" marginBottom="16px"
-            onChange={handleFormChange} value={selectedForm} className={selectedForm ? '' : 'lighter-text'}
-            backgroundColor="white"
-            icon={renderDropdownIcon()}
-            onFocus={handleDropdownFocus}
-            onBlur={handleDropdownBlur}>
-            <option value="">Select Form Template</option>
-            <option value="Form 1" onClick={handleDropdownClick}>Form 1</option>
-            <option value="Form 2" onClick={handleDropdownClick}>Form 2</option>
-            <option value="Form 3" onClick={handleDropdownClick}>Form 3</option>
-          </Select>
-          <Box width="496px" height="436px" backgroundColor="gray.300" marginBottom="10px">
+          <Select
+          selectedOptionStyle="check"
+            options={options}
+            placeholder="Select Form Template"
+            value={selectedForm}
+            onChange={setSelectedForm}
+          />
+          <Box width="496px" height="436px" backgroundColor="gray.300" marginBottom="10px" marginTop="10px">
             {/* Placeholder for PDF */}
           </Box>
         </Flex>
@@ -84,23 +71,23 @@ const CreateForm = () => {
           <Text fontWeight="500" fontSize="16px" color="black" marginTop="40px">
             Leadership Team Member
           </Text>
-          <Select width="440px" height="40px" marginTop="9px"
+          {/* <Select width="440px" height="40px" marginTop="9px"
             backgroundColor="white" icon={<DropdownDownArrow />}>
             <option selected hidden disabled value="">Select assignee</option>
             <option value="Form 1">First Last</option>
             <option value="Form 2">First Last</option>
             <option value="Form 3">First Last</option>
-          </Select>
+          </Select> */}
           <Text fontWeight="500" fontSize="16px" color="black" marginTop="24px">
             Department Head
           </Text>
-          <Select width="440px" height="40px" marginTop="9px"
+          {/* <Select width="440px" height="40px" marginTop="9px"
             backgroundColor="white" icon={<DropdownDownArrow />}>
             <option selected hidden disabled value="">Select assignee</option>
             <option value="Form 1">First Last</option>
             <option value="Form 2">First Last</option>
-            <option value="Form 3">First Last</option>
-          </Select>
+            <option value="Form 3">First Last</option> 
+        </Select> */}
         </Flex>
       </Flex>
 
