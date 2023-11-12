e44ded3ab51ead0f35b4d07b302da3393a566d1a
Elvin Cheng <elvincheng3@gmail.com>
2023-11-12 14:05:14 -0500

    lint

--- apps/server/src/positions/positions.controller.ts
+++ apps/server/src/positions/positions.controller.ts
@@ -22,7 +22,7 @@ import {
   ApiTags,
   ApiUnprocessableEntityResponse,
 } from '@nestjs/swagger';
-import { PositionBaseEntity, PositionEntity } from './entities/position.entity';
+import { PositionEntity } from './entities/position.entity';
 import { Prisma } from '@prisma/client';
 import { AppErrorMessage } from '../app.errors';
 import { PositionsErrorMessage } from './positions.errors';
