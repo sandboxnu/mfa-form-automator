357e99efb6b1615b922500be4288c01cc0131de2
Anshul Shirude <34725764+AnshulShirude@users.noreply.github.com>
2023-12-02 14:18:44 -0500

    it should work, but it doesn't

--- apps/web/src/components/FormLoading.tsx
+++ apps/web/src/components/FormLoading.tsx
@@ -1,7 +1,11 @@
-import React from 'react';
-import styles from '../styles/Home.module.css';
+import React, { useEffect } from 'react';
+import { lineSpinner } from 'ldrs';
 
 const FormLoading: React.FC = () => {
+  useEffect(() => {
+    lineSpinner.register();
+  }, []);
+
   return (
     <div
       style={{
@@ -19,13 +23,33 @@ const FormLoading: React.FC = () => {
           alignItems: 'center',
         }}
       >
-        <div className={styles.loader}></div>
+        <l-line-spinner
+          size="40"
+          stroke="3"
+          speed="1"
+          color="black"
+        ></l-line-spinner>
       </div>
-      <div>
-        <p style={{ paddingTop: '20px', fontSize: '24px', fontWeight: 500 }}>
-          Forms are loading...
-        </p>
-        <p style={{ paddingTop: '3px', fontSize: '18px', fontWeight: 400 }}>
+
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          paddingTop: '20px',
+          fontSize: '24px',
+          fontWeight: 500,
+        }}
+      >
+        <p>Forms are loading...</p>
+        <p
+          style={{
+            paddingTop: '3px',
+            fontSize: '18px',
+            fontWeight: 400,
+            textAlign: 'center',
+          }}
+        >
           Thank you for your patience.
         </p>
       </div>
