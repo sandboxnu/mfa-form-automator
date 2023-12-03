019af2dbce1c2361172790c0dbd610373e13b32b
Kaiyang Zheng <kaiyang.zheng@gmail.com>
2023-12-03 15:45:04 -0500

    mark as done/completed section (#70)
    * added form approval flow to frontend, also edited backend to only allow originators to approve forms
    
    * format
    
    * fixed form instance entity
    
    * generated prisma and web client
    
    * added guards for marking a form instance as completed - person marking completed must be the form originator or an admin in the same department in the department
    
    * fix tests
    
    * fix invalidateQueries

--- apps/server/src/form-instances/form-instance.errors.ts
+++ apps/server/src/form-instances/form-instance.errors.ts
@@ -3,4 +3,5 @@ export enum FormInstanceErrorMessage {
   FORM_INSTANCE_NOT_FOUND_CLIENT = 'Form instance could not be found',
   FORM_INSTANCE_INVALID_NUMBER_OF_SIGNATURES = 'Invalid number of signatures specified',
   FORM_INSTANCE_NOT_COMPLETED = 'Form instance has not been completely signed',
+  FORM_INSTANCE_INVALID_MARKED_COMPLETED = 'Form instance cannot be marked completed',
 }
