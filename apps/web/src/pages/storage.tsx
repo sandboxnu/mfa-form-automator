b4fc5c3ddc0e4039cf0d5ebb6cc67300c61d6f57
Kaiyang Zheng <kaiyang.zheng@gmail.com>
2024-03-05 17:33:38 -0800

    pass error

--- apps/web/src/pages/storage.tsx
+++ apps/web/src/pages/storage.tsx
@@ -11,7 +11,7 @@ export default function Storage() {
       <input
         type="file"
         onChange={(e) => {
-          const file = e.target.files[0];
+          const file = e.target.files?.[0];
           if (file) {
             uploadBlob(file);
           }
