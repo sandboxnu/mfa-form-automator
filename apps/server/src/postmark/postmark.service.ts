1c4f2e94cac617f1d89bcb3bbd1a4203ce59659a
Kaiyang Zheng <kaiyang.zheng@gmail.com>
2024-03-23 21:38:56 -0400

    retry postmark (#102)
    * retry postmark
    
    * add postmark
    
    * update gh yaml

---
+++ apps/server/src/postmark/postmark.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import { ServerClient } from 'postmark';
+
+@Injectable()
+export class PostmarkService {
+  client: ServerClient;
+
+  constructor() {
+    this.client = new ServerClient(process.env.POSTMARK_SERVER_KEY || '');
+  }
+
+  async sendEmail(to: string, subject: string, textBody: string) {
+    try {
+      this.client.sendEmail({
+        From: 'jfrederick@mfa.org',
+        To: to,
+        Subject: subject,
+        // TODO FIXME when this is uncommented the text body doesn't show up
+        // 'HtmlBody': '<strong>Hello < /strong> dear Postmark user.',
+        TextBody: textBody,
+        MessageStream: 'outbound',
+      });
+    } catch (error) {
+      console.error('Error sending email through postmark:', error);
+    }
+  }
+}
