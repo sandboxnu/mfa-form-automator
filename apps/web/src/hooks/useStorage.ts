e9745c3dcf05b0596639342f01c1b90ef1fa7fd8
Kaiyang Zheng <kaiyang.zheng@gmail.com>
2024-04-08 13:02:15 -0400

    storage works

--- apps/web/src/hooks/useStorage.ts
+++ apps/web/src/hooks/useStorage.ts
@@ -1,41 +1,27 @@
 import { FormInstanceEntity, FormTemplateEntity } from '@web/client';
 import { useEffect, useState } from 'react';
-import { mockStorage } from './../services/storage.service';
+import { storage } from './../services/storage.service';
 
 // hook to fetch form blob from storage
 export const useStorage = (
   form: FormInstanceEntity | FormTemplateEntity | null,
 ) => {
   const [formBlob, setFormBlob] = useState<Blob | null>(null);
-  const [mockBlob, setMockBlob] = useState<Blob | null>(null);
 
   // This needs to be uncommented once storage is set up
-  // useEffect(() => {
-  //   async function fetchFormBlob() {
-  //     const blob = (await storage.downloadBlob(form?.formDocLink!)) as Blob;
-  //     const arrayBuffer = await blob.arrayBuffer();
-  //     setFormBlob(new Blob([arrayBuffer], { type: 'application/pdf' }));
-  //   }
-
-  //   async function fetchMockBlob() {}
-
-  //   if (form) {
-  //     fetchFormBlob();
-  //   }
-  // }, [form]);
-
   useEffect(() => {
-    async function fetchMockBlob() {
-      const blob = (await mockStorage.downloadBlob('mockBlobLink')) as Blob;
+    async function fetchFormBlob() {
+      const blob = (await storage.downloadBlob(form?.formDocLink!)) as Blob;
       const arrayBuffer = await blob.arrayBuffer();
-      setMockBlob(new Blob([arrayBuffer], { type: 'application/pdf' }));
+      setFormBlob(new Blob([arrayBuffer], { type: 'application/pdf' }));
     }
 
-    fetchMockBlob();
+    if (form) {
+      fetchFormBlob();
+    }
   }, [form]);
 
   return {
     formBlob,
-    mockBlob,
   };
 };
