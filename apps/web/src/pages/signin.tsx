07fcbc33a783a996d45fb5e875430087a346a800
Kaiyang Zheng <kaiyang.zheng@gmail.com>
2024-09-09 20:25:06 -0400

    139-bug-secret-expired (#140)
    * Always show sign in button in case the user thats signed in does not have an account
    
    * Delete unused import from signin.tsx

--- apps/web/src/pages/signin.tsx
+++ apps/web/src/pages/signin.tsx
@@ -4,38 +4,36 @@ import { MFALogoIcon } from '@web/static/icons';
 import { useAuth } from '@web/hooks/useAuth';
 
 export default function Signin() {
-  const { session, signIn } = useAuth();
+  const { signIn } = useAuth();
 
-  if (!session) {
-    return (
-      <>
-        <Flex
-          justifyContent={'center'}
-          marginTop={'168px'}
-          marginBottom={'100px'}
+  return (
+    <>
+      <Flex
+        justifyContent={'center'}
+        marginTop={'168px'}
+        marginBottom={'100px'}
+      >
+        <Box
+          width="480px"
+          borderRadius="8px"
+          background="#FFF"
+          boxShadow="0px 0px 6px 1px #D4D4D4"
+          padding="48px"
         >
-          <Box
-            width="480px"
-            borderRadius="8px"
-            background="#FFF"
-            boxShadow="0px 0px 6px 1px #D4D4D4"
-            padding="48px"
-          >
-            <Flex justifyContent={'center'}>
-              <MFALogoIcon width="150px" height="30px" marginBottom="20px" />
-            </Flex>
-            <Flex justifyContent="center" marginTop="32px" marginBottom="48px">
-              <Button
-                background="#4C658A"
-                color="#FFF"
-                onClick={async () => signIn('azure-ad')}
-              >
-                Sign In with Azure Entra SSO
-              </Button>
-            </Flex>
-          </Box>
-        </Flex>
-      </>
-    );
-  }
+          <Flex justifyContent={'center'}>
+            <MFALogoIcon width="150px" height="30px" marginBottom="20px" />
+          </Flex>
+          <Flex justifyContent="center" marginTop="32px" marginBottom="48px">
+            <Button
+              background="#4C658A"
+              color="#FFF"
+              onClick={async () => signIn('azure-ad')}
+            >
+              Sign In with Azure Entra SSO
+            </Button>
+          </Flex>
+        </Box>
+      </Flex>
+    </>
+  );
 }
