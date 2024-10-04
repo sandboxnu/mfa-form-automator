3a51feca59f3bb6d4529fb89cf84c4bce18f2d05
DonnyLe <98658081+DonnyLe@users.noreply.github.com>
2024-10-03 21:43:47 -0400

    Fixed CSS to match Figma

--- apps/web/src/components/FormImageCard.tsx
+++ apps/web/src/components/FormImageCard.tsx
@@ -43,81 +43,81 @@ export const FormImageCard = ({
   };
 
   return (
-    <>
+    <Box
+      minW="246px"
+      minH="120px"
+      borderRadius="5px"
+      backgroundColor="#FFFFFF"
+      boxShadow="0px 0.5px 3px 1px #DCDCDC"
+      background="#FCFCFC"
+      cursor="pointer"
+      onClick={() => {
+        router.push(link);
+      }}
+      _hover={{
+        boxShadow: '0px 0.5px 6px 1px #DCDCDC',
+      }}
+    >
       <Box
-        minW="246px"
-        minH="120px"
-        borderRadius="5px"
-        backgroundColor="#FFFFFF"
-        boxShadow="0px 0.5px 3px 1px #DCDCDC"
-        background="#FCFCFC"
-        cursor="pointer"
-        onClick={() => {
-          router.push(link);
-        }}
-        _hover={{
-          boxShadow: '0px 0.5px 6px 1px #DCDCDC',
-        }}
+        maxH="132px"
+        borderBottom="2px solid rgb(225,225,225)"
+        padding="16px"
       >
-        <Box>
-          <Box borderBottom="2px solid rgb(225,225,225)" padding="16px">
-            <Box overflow="hidden" height="200px" filter="blur(2px)">
-              <Document file={formURL}>
-                <Page
-                  renderAnnotationLayer={false}
-                  renderTextLayer={false}
-                  pageNumber={1}
-                  width={400}
-                />
-              </Document>
-            </Box>
-          </Box>
-          <Flex
-            margin="15px"
-            height="75px"
-            flexDirection="column"
-            justifyContent="space-between"
-          >
-            <Text
-              padding={0}
-              margin={0}
-              fontFamily="Hanken Grotesk"
-              fontWeight={800}
-              fontSize="18px"
-              isTruncated
-            >
-              {formInstance.name}
-            </Text>
-            <Flex display="flex" justifyContent="space-between">
-              <Text
-                textAlign="center"
-                width="7vh"
-                backgroundColor="#FFDFDE"
-                fontFamily="Hanken Grotesk"
-                fontWeight={800}
-                fontSize="18px"
-                isTruncated
-                borderRadius="24px"
-              >
-                TO DO
-              </Text>
-              <Text
-                textAlign="center"
-                color="#5E5E5E"
-                as="i"
-                fontFamily="Hanken Grotesk"
-                fontWeight={800}
-                fontSize="18px"
-                isTruncated
-              >
-                Assigned{' '}
-                {daysBetween(formInstance.createdAt, new Date().toISOString())}d
-                ago
-              </Text>
-            </Flex>
-          </Flex>
+        <Box overflow="hidden" height="200px" filter="blur(2px)">
+          <Document file={formURL}>
+            <Page
+              renderAnnotationLayer={false}
+              renderTextLayer={false}
+              pageNumber={1}
+              width={400}
+            />
+          </Document>
         </Box>
       </Box>
-    </>
+      <Flex
+        margin="15px"
+        height="75px"
+        flexDirection="column"
+        justifyContent="space-between"
+      >
+        <Text
+          padding={0}
+          margin={0}
+          fontFamily="Hanken Grotesk"
+          fontWeight={500}
+          fontSize="15px"
+          isTruncated
+        >
+          {formInstance.name}
+        </Text>
+        <Flex display="flex" justifyContent="space-between" alignItems="center">
+          <Text
+            textAlign="center"
+            width="auto"
+            backgroundColor="#FFDFDE"
+            fontFamily="Hanken Grotesk"
+            fontWeight={700}
+            fontSize="12px"
+            isTruncated
+            borderRadius="20px"
+            padding="3px 12px"
+          >
+            TO DO
+          </Text>
+          <Text
+            textAlign="center"
+            color="#5E5E5E"
+            as="i"
+            fontFamily="Hanken Grotesk"
+            fontWeight={500}
+            fontSize="13px"
+            isTruncated
+          >
+            Assigned{' '}
+            {daysBetween(formInstance.createdAt, new Date().toISOString())}d ago
+          </Text>
+        </Flex>
+      </Flex>
+    </Box>
   );
 };
