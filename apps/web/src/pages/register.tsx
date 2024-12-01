11486518e043804eac34250fceeba9ec0a3a7c6a
Kaiyang Zheng <kaiyang.zheng@gmail.com>
2024-11-30 22:43:38 -0500

    Remove code

--- apps/web/src/pages/register.tsx
+++ apps/web/src/pages/register.tsx
@@ -1,4 +1,4 @@
-import { Flex, Box, Text, Select, Button, HStack } from '@chakra-ui/react';
+import { Flex, Box, Text, Select, Button } from '@chakra-ui/react';
 import { DepartmentsService, PositionsService } from '@web/client';
 import { DepartmentEntity } from '@web/client/models/DepartmentEntity';
 import { PositionEntity } from '@web/client/models/PositionEntity';
