fec3613a750edfd5ade5ea7e3b0d31fd3175f07a
DonnyLe <98658081+DonnyLe@users.noreply.github.com>
2024-12-01 22:32:14 -0500

    prettier fix

--- apps/web/src/components/createFormTemplate/createFormTemplateEditor/DraggableSignature.tsx
+++ apps/web/src/components/createFormTemplate/createFormTemplateEditor/DraggableSignature.tsx
@@ -1,11 +1,15 @@
-import Draggable, { DraggableEventHandler } from "react-draggable";
+import Draggable, { DraggableEventHandler } from 'react-draggable';
 
-
-export default function DraggableSignature({ onEnd, onSet, onCancel, url}: {
-  onEnd: DraggableEventHandler,
-  onSet: () => void,
-  onCancel: () => void
-  url: string,
+export default function DraggableSignature({
+  onEnd,
+  onSet,
+  onCancel,
+  url,
+}: {
+  onEnd: DraggableEventHandler;
+  onSet: () => void;
+  onCancel: () => void;
+  url: string;
 }) {
   const styles = {
     container: {
@@ -17,21 +21,25 @@ export default function DraggableSignature({ onEnd, onSet, onCancel, url}: {
       Position: 'absolute',
       right: 0,
       display: 'inline-block',
-      backgroundColor: "white",
+      backgroundColor: 'white',
       // borderRadius: 4,
     },
     smallButton: {
       display: 'inline-block',
       cursor: 'pointer',
       padding: 4,
-    }
-  }
+    },
+  };
   return (
     <Draggable onStop={onEnd}>
       <div style={styles.container}>
         <div style={styles.controls}>
-          <div style={styles.smallButton} onClick={onSet}>Set </div>
-          <div style={styles.smallButton} onClick={onCancel}>Cancel</div>
+          <div style={styles.smallButton} onClick={onSet}>
+            Set{' '}
+          </div>
+          <div style={styles.smallButton} onClick={onCancel}>
+            Cancel
+          </div>
         </div>
       </div>
     </Draggable>
