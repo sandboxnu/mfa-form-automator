d2d820c6cf08dbba73a4d107f306ff8189c8dd05
Elvin Cheng <elvincheng3@gmail.com>
2025-01-23 15:18:45 -0500

    Fix prisma import

--- apps/server/src/signatures/signatures.module.ts
+++ apps/server/src/signatures/signatures.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { SignaturesService } from './signatures.service';
 import { SignaturesController } from './signatures.controller';
 import { LoggerModule } from '../logger/logger.module';
-import { PrismaModule } from '@server/prisma/prisma.module';
+import { PrismaModule } from '../prisma/prisma.module';
 import { EmployeesModule } from '../employees/employees.module';
 import { DepartmentsModule } from '../departments/departments.module';
 import { PositionsModule } from '../positions/positions.module';
