9d0e4b0ea3306f66b06ebb2705a1cb813700fca2
Lauren Brissette <168485735+laurenbrissette@users.noreply.github.com>
2025-01-25 16:09:19 -0500

    add employee field throughout files
    Co-authored-by: Gayatri Kondabathini <Gayatri-K26@users.noreply.github.com>

--- apps/server/src/metadata.ts
+++ apps/server/src/metadata.ts
@@ -107,6 +107,7 @@ export default async () => {
               positionId: { required: true, type: () => String },
               email: { required: true, type: () => String },
               password: { required: true, type: () => String, minLength: 5 },
+              isAdmin: { required: true, type: () => Boolean },
             },
           },
         ],
