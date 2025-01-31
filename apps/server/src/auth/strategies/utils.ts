5950d757002da6c122fc564fd98a41ec6c44223e
Kaiyang Zheng <kaiyang.zheng@gmail.com>
2025-01-31 00:32:37 -0500

    test any type

--- apps/server/src/auth/strategies/utils.ts
+++ apps/server/src/auth/strategies/utils.ts
@@ -1,7 +1,5 @@
-import { Request as RequestType } from 'express';
-
 export class StrategyUtils {
-  public static extractJWT(req: RequestType): string | null {
+  public static extractJWT(req: any): string | null {
     if (req.cookies && 'jwt' in req.cookies && req.cookies.jwt.length > 0) {
       return req.cookies.jwt;
     }
