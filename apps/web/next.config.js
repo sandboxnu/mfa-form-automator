04e821cb5ca0181ef9e8b7430dcf4c232275e223
Elvin Cheng <elvincheng3@gmail.com>
2025-02-06 12:07:44 -0500

    set up turbopack

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,25 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    /**
-     * Critical: prevents "ESM packages (pdfjs-dist/build/pdf.worker.min.mjs) need to be imported." error
-     */
-    esmExternals: 'loose',
     // You may not need this, it's just to support moduleResolution: 'node16'
     extensionAlias: {
       '.js': ['.tsx', '.ts', '.jsx', '.js'],
     },
-    turbo: {
-      resolveAlias: {
-        // Turbopack does not support standard ESM import paths yet
-        './Sample.js': './pages/Sample.tsx',
-        /**
-         * Critical: prevents " ⨯ ./node_modules/canvas/build/Release/canvas.node
-         * Module parse failed: Unexpected character '�' (1:0)" error
-         */
-        canvas: './empty-module.ts',
-      },
-    },
+    turbo: {}
   },
   webpack: (config) => {
     /**
