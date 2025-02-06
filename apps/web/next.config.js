1fc57f4c443c4b93aff8c019834e2043fe00ec32
Elvin Cheng <elvincheng3@gmail.com>
2025-02-06 13:48:47 -0500

    remove webpack config

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -7,15 +7,6 @@ const nextConfig = {
     },
     turbo: {},
   },
-  webpack: (config) => {
-    /**
-     * Critical: prevents " ⨯ ./node_modules/canvas/build/Release/canvas.node
-     * Module parse failed: Unexpected character '�' (1:0)" error
-     */
-    config.resolve.alias.canvas = false;
-
-    return config;
-  },
   reactStrictMode: true,
   env: {
     PORT: process.env.FRONTEND_PORT,
