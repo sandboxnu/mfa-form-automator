f166ef07f45bbdeb3f7a7959fd49be3b2c160ea8
Kaiyang Zheng <kaiyang.zheng@gmail.com>
2025-02-06 19:13:42 -0500

    merge from main, revert jwt strategy changes

--- apps/server/src/auth/strategies/utils.ts
+++ apps/server/src/auth/strategies/utils.ts
@@ -1,5 +1,7 @@
+import { Request as RequestType } from 'express';
+
 export class StrategyUtils {
-  public static extractJWT(req: any): string | null {
+  public static extractJWT(req: RequestType): string | null {
     if (req.cookies && 'jwt' in req.cookies && req.cookies.jwt.length > 0) {
       return req.cookies.jwt;
     }
