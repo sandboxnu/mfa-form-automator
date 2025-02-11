433dfa36a95ab5708ed16088cc71adc30b56e806
Elvin Cheng <elvincheng3@gmail.com>
2025-02-11 15:30:57 -0500

    Fix tests

--- apps/server/src/form-templates/form-templates.service.spec.ts
+++ apps/server/src/form-templates/form-templates.service.spec.ts
@@ -1,6 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { FormTemplatesService } from './form-templates.service';
 import { PrismaService } from './../../../server/src/prisma/prisma.service';
+import { PdfStoreService } from '../pdf-store/pdf-store.service';
+import { VercelFileStorageHandler } from '../pdf-store/file-storage/VercelFileStorageHandler';
+import { PdfStoreModule } from '../pdf-store/pdf-storage.module';
 
 describe('FormTemplatesService', () => {
   let service: FormTemplatesService;
@@ -8,6 +11,7 @@ describe('FormTemplatesService', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [FormTemplatesService, PrismaService],
+      imports: [PdfStoreModule],
     }).compile();
 
     service = module.get<FormTemplatesService>(FormTemplatesService);
