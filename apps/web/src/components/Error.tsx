3f88438a9e9c5f4d54f52ae51a921a6ddb0ece79
Elvin Cheng <elvincheng3@gmail.com>
2025-03-30 21:32:19 -0400

    add auth to form instance pages (#349)

--- apps/web/src/components/Error.tsx
+++ apps/web/src/components/Error.tsx
@@ -4,16 +4,22 @@ import { ErrorIcon } from 'apps/web/src/static/icons.tsx';
 /**
  * @returns an error message when the page fails to load the forms
  */
-const Error: React.FC = () => {
+type ErrorPropType = {
+  primaryErrorMessage?: string;
+  secondaryErrorMessage?: string;
+};
+
+const Error: React.FC<ErrorPropType> = (props: ErrorPropType) => {
   return (
     <div style={{ textAlign: 'center', paddingTop: '144px' }}>
       <ErrorIcon width="44px" height="44px" />
       <div>
         <p style={{ paddingTop: '20px', fontSize: '24px', fontWeight: 500 }}>
-          Oops! Something went wrong.
+          {props.primaryErrorMessage ?? 'Oops! Something went wrong.'}
         </p>
         <p style={{ paddingTop: '3px', fontSize: '18px', fontWeight: 400 }}>
-          This page failed to load the forms. Please try again later.
+          {props.secondaryErrorMessage ??
+            'This page failed to load the forms. Please try again later.'}
         </p>
       </div>
     </div>
