0d7fea1b701d1aa6c5ccefa63249019210c415d0
Elvin Cheng <elvincheng3@gmail.com>
2025-04-07 14:37:10 -0400

    try yet again

--- apps/web/docker-entrypoint.sh
+++ apps/web/docker-entrypoint.sh
@@ -3,5 +3,5 @@
 # Replace the placeholder in nginx.conf with actual value
 envsubst '${API_URL}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
-# Start nginx
-exec nginx -g 'daemon off;'
+# Start supervisord
+exec "$@"
