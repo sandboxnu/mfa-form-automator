63de5e3ce3dd39a82099d6deb16276cc04ebfbf2
Elvin Cheng <elvincheng3@gmail.com>
2025-04-07 19:58:51 -0400

    add default url

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -14,7 +14,7 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: `${process.env.API_URL}/api/:path*`,
+        destination: `${process.env.API_URL ?? 'https://localhost:8080'}/api/:path*`,
       },
     ];
   },
