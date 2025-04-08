64ad68b004743d98cded16b66e0861ea3dfd0018
Elvin Cheng <elvincheng3@gmail.com>
2025-04-07 20:17:29 -0400

    format

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -14,9 +14,7 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: `${
-          process.env.API_URL
-        }/api/:path*`,
+        destination: `${process.env.API_URL}/api/:path*`,
       },
     ];
   },
