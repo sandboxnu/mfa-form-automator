0af277d33453bf5a34bd1be662972c44e4f75e19
Elvin Cheng <elvincheng3@gmail.com>
2025-04-07 21:11:30 -0400

    update turbo json

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -8,9 +8,6 @@ const nextConfig = {
     turbo: {},
     optimizePackageImports: ['@chakra-ui/react'],
   },
-  env: {
-    API_URL: process.env.API_URL,
-  },
   reactStrictMode: true,
   output: 'standalone',
   async rewrites() {
