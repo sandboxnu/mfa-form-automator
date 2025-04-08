df6090e5722a565c0e1f4f71446847e27750e235
Elvin Cheng <elvincheng3@gmail.com>
2025-04-07 21:08:20 -0400

    try setting the env in next.config?

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -8,6 +8,9 @@ const nextConfig = {
     turbo: {},
     optimizePackageImports: ['@chakra-ui/react'],
   },
+  env: {
+    API_URL: process.env.API_URL,
+  },
   reactStrictMode: true,
   output: 'standalone',
   async rewrites() {
