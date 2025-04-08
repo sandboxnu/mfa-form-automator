c241ee01870213a8c0ec6a0874fd5d9548dbd04c
Elvin Cheng <elvincheng3@gmail.com>
2025-04-07 21:38:43 -0400

    try this http proxy approach

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -10,14 +10,15 @@ const nextConfig = {
   },
   reactStrictMode: true,
   output: 'standalone',
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: `${process.env.API_URL}/api/:path*`,
-      },
-    ];
-  },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: '/api/:path*',
+  //       destination: `${process.env.API_URL}/api/:path*`,
+  //       basePath: false,
+  //     },
+  //   ];
+  // },
   env: {
     PORT: process.env.FRONTEND_PORT,
     AZURE_CLIENT_ID: process.env.AZURE_CLIENT_ID,
