30677517315a5a88076e4d262983fcf6d47af033
Kaiyang Zheng <kaiyang.zheng@gmail.com>
2025-05-02 03:11:45 -0700

    new migrations last minute!

---
+++ apps/server/prisma/migrations/20250502094926_increase_length/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "FormTemplate" ALTER COLUMN "formDocLink" SET DATA TYPE VARCHAR(1000);
---
+++ apps/server/prisma/migrations/20250502095747_templates_max_length/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "FormInstance" ALTER COLUMN "formDocLink" SET DATA TYPE VARCHAR(1000);
